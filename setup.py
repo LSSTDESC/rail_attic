c586c615febd9f320955aa0ddef4b99a614931b6
Johann <johann.cohentenugi@gmail.com>
2020-09-17 10:17:03 +0200

    remove the need to add packages.append by adding include_package_data=True. Add url

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 from setuptools import setup, find_packages
 
 packages = find_packages()
-packages.append('rail.estimation.tests.data')
-print(packages)
 setup(
     name='rail',
     version='0.1.dev0',
@@ -11,8 +9,10 @@ setup(
     author_email='aimalz@nyu.edu',
     packages=packages,
     package_data={"": ["*.hdf5","*.yaml"],"tests":["*.hdf5","*.yaml"],},
+    include_package_data=True,
     license='BSD 3-Clause License',
     description="Redshift Assessment Infrastructure Layers",
+    url="https://github.com/LSSTDESC/RAIL",
     long_description=open("README.md").read(),
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -29,5 +29,4 @@ setup(
     python_requires='>=3.5',
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-
 )
