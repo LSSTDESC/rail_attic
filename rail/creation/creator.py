d53415b47a165afa20c8d7be7054b0d9f26b34f2
Bryce Kalmbach <jbkalmbach@gmail.com>
2020-11-30 14:32:01 -0800

    Sprint Day 1 progress: Updated creator.py to make more general. Working on adding tests to test_creator.py.

--- rail/creation/creator.py
+++ rail/creation/creator.py
@@ -8,7 +8,7 @@ class Creator():
     with an optional selection function applied.
 
     generator - an object defining a redshift probability distribution. Must have
-                a sample and log_prob methods.
+                a sample, log_prob and pz_estimate methods (see generator.py)
     selection_fn - a selection function to apply to the generated sample
     params - additional information desired to be stored with the instance as a dictionary
     """
@@ -16,48 +16,66 @@ class Creator():
     def __init__(self, generator, selection_fn=None, params=None):
         self.generator = generator
         self.selection_fn = selection_fn
-        self.params = params
 
-    def sample(self, n_samples, seed=None, include_err=False, include_pdf=False, zmin=0, zmax=2, dz=0.02):
+        param_defaults = {}
+        param_defaults['bands'] = ['u', 'g', 'r', 'i', 'z', 'y']
+
+        # 10 yr limiting mags from https://www.lsst.org/scientists/keynumbers
+        # currently these limitting mags are set 2 mags
+        # dimmer than actual LSST vals
+        param_defaults['err_params'] = {'gamma_u': 0.038,
+                                        'gamma_g': 0.039,
+                                        'gamma_r': 0.039,
+                                        'gamma_i': 0.039,
+                                        'gamma_z': 0.039,
+                                        'gamma_y': 0.039,
+                                        'm5_u': 28.1,
+                                        'm5_g': 29.4,
+                                        'm5_r': 29.5,
+                                        'm5_i': 28.8,
+                                        'm5_z': 28.1,
+                                        'm5_y': 26.9}
+
+        if params is None:
+            self.params = {}
+            self.params['bands'] = param_defaults['bands']
+            self.params['err_params'] = param_defaults['err_params']
+        else:
+            self.params = params
+            param_keys = self.params.keys()
+            for key_name in ['bands', 'err_params']:
+                if key_name not in param_keys:
+                    self.params[key_name] = param_defaults[key_name]
+
+    def sample(self, n_samples, seed=None, include_err=False,
+               include_pdf=False, zmin=0, zmax=2, dz=0.02):
         """
-        Draw n_samples from the generator. 
+        Draw n_samples from the generator.
         If include_pdf == True, then a conditional pdf for redshift will be returned with each
         set of galaxy magnitudes. The pdf is evaluated at points in np.arange(zmin, zmax+dz, dz).
         seed sets the random seed for drawing samples.
         """
-        
+
         # get samples
         sample = self.generator.sample(n_samples, seed=seed)
-        
+
         if include_err:
             # add errors to the sample
             # using Eq 5 from https://arxiv.org/pdf/0805.2366.pdf
-            # and 10 yr limiting mags from https://www.lsst.org/scientists/keynumbers
-            # currently these limitting mags are set 2 mags dimmer than actual LSST vals
-            err_params = {'gamma_u':0.038,
-                          'gamma_g':0.039,
-                          'gamma_r':0.039,
-                          'gamma_i':0.039,
-                          'gamma_z':0.039,
-                          'gamma_y':0.039,
-                          'm5_u':28.1,
-                          'm5_g':29.4,
-                          'm5_r':29.5,
-                          'm5_i':28.8,
-                          'm5_z':28.1,
-                          'm5_y':26.9}
 
-            for band in ['u','g','r','i','z','y']:
-                gamma = err_params[f'gamma_{band}']
-                m5 = err_params[f'm5_{band}']
+            for band in self.params['bands']:
+                gamma = self.params['err_params'][f'gamma_{band}']
+                m5 = self.params['err_params'][f'm5_{band}']
                 x = 10**(0.4*(sample[band]-m5))
-                sample[f'{band}err'] = np.sqrt( (0.04 - gamma) * x + gamma * x**2 )
-                sample[band] = np.random.normal(sample[band], sample[f'{band}err'])
-        
+                sample[f'{band}err'] = np.sqrt((0.04 - gamma) * x + gamma * x**2)
+                sample[band] = np.random.normal(sample[band],
+                                                sample[f'{band}err'])
+
         # calculate conditional pdfs
-        if include_pdf: 
-            posteriors = self.generator.pz_estimate(sample, zmin=zmin, zmax=zmax, dz=dz, convolve_err=include_err)
+        if include_pdf:
+            posteriors = self.generator.pz_estimate(
+                sample, zmin=zmin, zmax=zmax, dz=dz, convolve_err=include_err
+            )
             sample['pz_pdf'] = list(posteriors)
 
         return sample
-
--- tests/creation/test_creator.py
+++ tests/creation/test_creator.py
@@ -1,5 +1,6 @@
-import unittest
+import pytest
 import numpy as np
+import pandas as pd
 from rail.creation import Creator
 
 
@@ -11,27 +12,44 @@ class sampleGenerator(object):
 
         random_state = np.random.RandomState(seed=seed)
 
-        return random_state.normal(size=n_samples)
+        return pd.DataFrame(random_state.normal(size=(n_samples, 7)))
+
+    def pz_estimate(self, data, zmin, zmax, dz, convolve_err=False):
+        "Return defined set of posteriors for testing"
+
+        z_steps = np.arange(zmin, zmax+dz, dz)
+        output_array = np.ones((len(data), len(z_steps)))
+        test_posterior = output_array*(1./len(z_steps))
+
+        if convolve_err is False:
+            return test_posterior
 
 
-class testCreator(unittest.TestCase):
+@pytest.fixture
+def generator():
+    # Instantiate Creator
+    sample_gen = sampleGenerator()
+    test_creator_obj = Creator(sample_gen)
+    return test_creator_obj
 
-    def test_creator(self):
-        "Test that sample works with seed to create reproducible results"
 
-        # Instantiate Creator
-        sample_gen = sampleGenerator()
-        test_creator_obj = Creator(sample_gen)
+def test_sample_with_all_options_false(generator):
+    "Test that sample works with seed to create reproducible results"
 
-        # Sample from Creator object
-        creator_sample = test_creator_obj.sample(1000, seed=42)
+    # Sample from Creator object
+    creator_sample = generator.sample(1000, seed=42)
 
-        # Create copy of sampler from sampleGenerator
-        verify_sampler = np.random.RandomState(seed=42)
+    # Create copy of sampler from sampleGenerator
+    verify_sampler = np.random.RandomState(seed=42)
+    verify_sampler_df = pd.DataFrame(verify_sampler.normal(size=(1000, 7)))
 
-        # Verify samples with same seed and size are the same
-        np.testing.assert_array_equal(creator_sample, verify_sampler.normal(size=1000))
+    # Verify samples with same seed and size are the same
+    np.testing.assert_array_equal(creator_sample, verify_sampler_df)
 
-if __name__ == "__main__":
 
-    unittest.main()
+def test_sample_with_pdf_true(generator):
+    "Test that sample returns pdfs correctly when wanted"
+    pdf_sample = generator.sample(1000, seed=42, include_pdf=True,
+                                  zmin=0, zmax=2, dz=0.5)
+    np.testing.assert_array_equal(np.stack(pdf_sample['pz_pdf'].values),
+                                  np.ones((1000, 5))*0.2)
