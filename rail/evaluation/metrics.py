fbe6a85943bd45e6f0acd787fd7ed8757bd7a312
Julia Gschwend <julia@linea.gov.br>
2020-11-30 15:08:56 -0800

    Initial commit: new file metrics.py.

---
+++ rail/evaluation/metrics.py
@@ -0,0 +1,10 @@
+import os
+#from rail.estimation.utils import load_training_data, get_input_data_size_hdf5
+import yaml
+
+
+class Metrics(self):
+    """The base class for photo-z quality metrics evaluation.""" 
+
+    raise NotImplementedError
+
