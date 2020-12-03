50904b2d758fc88ab8934b414aba78050a2ade11
John Franklin Crenshaw <jfc20@uw.edu>
2020-12-02 22:28:39 -0800

    Removed the biased flow and implemented the bias as a selection function in the creator. Created training and test sets to be used in the estimation module.

--- rail/creation/creator.py
+++ rail/creation/creator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 class Creator():
@@ -9,8 +10,11 @@ class Creator():
 
     generator - an object defining a redshift probability distribution. Must have
                 a sample, log_prob and pz_estimate methods (see generator.py)
-    selection_fn - a selection function to apply to the generated sample
-    params - additional information desired to be stored with the instance as a dictionary
+    selection_fn - a selection function to apply to the generated sample. Must take a pandas
+                dataframe and a seed number, and return a pandas dataframe with the selection
+                function applied.
+    params - additional information desired to be stored with the instance as a dictionary.
+                By default, includes error params for Eq 5 from https://arxiv.org/pdf/0805.2366.pdf
     """
 
     def __init__(self, generator, selection_fn=None, params=None):
@@ -20,9 +24,9 @@ class Creator():
         param_defaults = {}
         param_defaults['bands'] = ['u', 'g', 'r', 'i', 'z', 'y']
 
+        # err params from Table 2 from https://arxiv.org/pdf/0805.2366.pdf
         # 10 yr limiting mags from https://www.lsst.org/scientists/keynumbers
-        # currently these limitting mags are set 2 mags
-        # dimmer than actual LSST vals
+        # currently these limitting mags are set 2 mags dimmer than actual LSST vals
         param_defaults['err_params'] = {'gamma_u': 0.038,
                                         'gamma_g': 0.039,
                                         'gamma_r': 0.039,
@@ -35,12 +39,13 @@ class Creator():
                                         'm5_i': 28.8,
                                         'm5_z': 28.1,
                                         'm5_y': 26.9}
-        print(param_defaults)
 
+        # if no params provided, use defaults
         if params is None:
             self.params = {}
             self.params['bands'] = param_defaults['bands']
             self.params['err_params'] = param_defaults['err_params']
+        # if params are provided, fill in defaults if bands and err_params not included
         else:
             self.params = params
             param_keys = self.params.keys()
@@ -58,28 +63,51 @@ class Creator():
                include_pdf=False, zmin=0, zmax=2, dz=0.02):
         """
         Draw n_samples from the generator.
-        If include_pdf == True, then a conditional pdf for redshift will be returned with each
-        set of galaxy magnitudes. The pdf is evaluated at points in np.arange(zmin, zmax+dz, dz).
-        seed sets the random seed for drawing samples.
+        include_err - if True, errors are calculated using Eq 5 from https://arxiv.org/pdf/0805.2366.pdf
+        include_pdf - if True, then posteriors are returned for each galaxy. The posteriors are saved
+                        in the column pz_pdf, and the redshift grid saved as df.attrs['pz_grid'].
+                        The grid is defined by np.arange(zmin, zmax+dz, dz)
+        seed - sets the random seed for drawing samples
+        err_seed - sets the seed for the Gaussian errors
         """
+        
+        rng = np.random.default_rng(seed)
 
         # get samples
-        sample = self.generator.sample(n_samples, seed=seed)
-
+        sample = self.generator.sample(n_samples, seed=rng.integers(1e18))
+        
+        
+        if self.selection_fn:
+            # apply selection function
+            sample = self.selection_fn(sample, seed=rng.integers(1e18))
+            # calculate fraction that survives the cut
+            selected_frac = len(sample)/n_samples
+            # draw more samples and cut until we have enough samples
+            while len(sample) < n_samples:
+                # estimate how many extras to draw
+                n_supplement = int( 1.1/selected_frac * (n_samples - len(sample)) )
+                # draw new samples and apply cut
+                new_sample = self.generator.sample(n_supplement, seed=rng.integers(1e18))
+                new_sample = self.selection_fn(new_sample, seed=rng.integers(1e18))
+                # add these to the larger set
+                sample = pd.concat((sample, new_sample), ignore_index=True)
+            # cut out the extras
+            sample = sample.iloc[:n_samples]
+            
         if include_err:
             # add errors to the sample
             # using Eq 5 from https://arxiv.org/pdf/0805.2366.pdf
-            rand_state = np.random.RandomState(seed=err_seed)
+            rng = np.random.default_rng(err_seed)
 
+            # calculate error in each band, then add Gaussian errors
             for band in self.params['bands']:
                 gamma = self.params['err_params'][f'gamma_{band}']
                 m5 = self.params['err_params'][f'm5_{band}']
                 x = 10**(0.4*(sample[band]-m5))
                 sample[f'{band}err'] = np.sqrt((0.04 - gamma) * x + gamma * x**2)
-                sample[band] = rand_state.normal(sample[band],
-                                                 sample[f'{band}err'])
+                sample[band] = rng.normal(sample[band], sample[f'{band}err'])
 
-        # calculate conditional pdfs
+        # calculate posteriors
         if include_pdf:
             nfeatures = len(self.params['bands']) + 1
             posteriors = self.generator.pz_estimate(sample.iloc[:,:nfeatures], zmin=zmin, zmax=zmax, dz=dz)
