aadc7526ecda2dbfbd553168fa8592e7fe4f5dce
pwhatfield <pwhatfield449@gmail.com>
2020-12-03 16:00:05 +0000

    Create GPz_classifier.py
    Currently just a copy of randomPZ

---
+++ rail/estimation/algos/GPz_classifier.py
@@ -0,0 +1,55 @@
+# This will eventaully be the GPz classifier...
+
+"""
+Example code that just spits out random numbers between 0 and 3
+for z_mode, and Gaussian centered at z_mode with width
+random_width*(1+zmode).
+"""
+
+import numpy as np
+from scipy.stats import norm
+from rail.estimation.estimator import Estimator as BaseEstimation
+
+
+class randomPZ(BaseEstimation):
+
+    def __init__(self, base_config, config_dict):
+        """
+        Parameters:
+        -----------
+        run_dict: dict
+          dictionary of all variables read in from the run_params
+          values in the yaml file
+        """
+        super().__init__(base_config=base_config, config_dict=config_dict)
+
+        inputs = self.config_dict['run_params']
+
+        self.width = inputs['rand_width']
+        self.zmin = inputs['rand_zmin']
+        self.zmax = inputs['rand_zmax']
+        self.nzbins = inputs['nzbins']
+        np.random.seed(87)
+
+    def inform(self):
+        """
+          this is random, so does nothing
+        """
+        print("I don't need to train!!!")
+        pass
+
+    def estimate(self, test_data):
+        pdf = []
+        # allow for either format for now
+        try:
+            d = test_data['i_mag']
+        except Exception:
+            d = test_data['mag_i_lsst']
+        numzs = len(d)
+        zmode = np.round(np.random.uniform(0.0, self.zmax, numzs), 3)
+        widths = self.width * (1.0 + zmode)
+        self.zgrid = np.linspace(0., self.zmax, self.nzbins)
+        for i in range(numzs):
+            pdf.append(norm.pdf(self.zgrid, zmode[i], widths[i]))
+        pz_dict = {'zmode': zmode, 'pz_pdf': pdf}
+        return pz_dict
