7e1a23f60788b06b224a179d27e3089fd22fc7bc
sschmidt23 <sschmidt@physics.ucdavis.edu>
2020-12-04 17:38:42 -0800

    fix linting

--- rail/estimation/algos/trainZ.py
+++ rail/estimation/algos/trainZ.py
@@ -9,6 +9,7 @@ N (z) of the training set.
 import numpy as np
 from rail.estimation.estimator import Estimator as BaseEstimation
 
+
 class trainZ(BaseEstimation):
 
     def __init__(self, base_config, config_dict):
@@ -19,21 +20,22 @@ class trainZ(BaseEstimation):
         self.zmax = inputs['zmax']
         self.nzbins = inputs['nzbins']
 
-        zbins = np.linspace(self.zmin,self.zmax,self.nzbins+1)
+        zbins = np.linspace(self.zmin, self.zmax, self.nzbins+1)
         speczs = np.sort(self.training_data['redshift'])
-        self.train_pdf,_ = np.histogram(speczs,zbins)
+        self.train_pdf, _ = np.histogram(speczs, zbins)
         self.midpoints = zbins[:-1] + np.diff(zbins)/2
         self.zmode = self.midpoints[np.argmax(self.train_pdf)]
         cdf = np.cumsum(self.train_pdf)
         self.cdf = cdf / cdf[-1]
         self.zgrid = self.midpoints
         np.random.seed(87)
-        
+
     def inform(self):
         pass
-    
-    def estimate(self,test_data):
+
+    def estimate(self, test_data):
         test_size = len(test_data['id'])
         zmode = np.repeat(self.zmode, test_size)
-        pz_dict = {'zmode': zmode, 'pz_pdf': np.tile(self.train_pdf,(test_size,1))}
+        pz_dict = {'zmode': zmode, 'pz_pdf': np.tile(self.train_pdf,
+                                                     (test_size, 1))}
         return pz_dict
