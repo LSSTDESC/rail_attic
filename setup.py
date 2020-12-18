cc6d9b71b7cc599301c8b0f2da912328754f50a2
sschmidt23 <sschmidt@physics.ucdavis.edu>
2020-12-17 16:00:54 -0800

    add qp to setup

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
                       'pyarrow',
                       'tables',
                       'astropy',
+                      'qp',
                       ],
     extras_require={
         'Full': ['sklearn', 'FlexCode[all]', 'corner', 'dill', 'flax==0.2.0',
