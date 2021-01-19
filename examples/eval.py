1d57faa3d403410c233c2a4ed5b63d61e570083f
Julia Gschwend <julia@linea.gov.br>
2021-01-18 22:08:26 -0300

    Add read data step to command line mode

--- examples/eval.py
+++ examples/eval.py
@@ -1,6 +1,7 @@
 import sys
-#from rail.evaluation.sample import Sample
-#from rail.evaluation.metrics import Metrics  # import metrics subclasses explicitly?
+from rail.evaluation.sample import Sample
+from rail.evaluation.metrics import 
+#Metrics  # import metrics subclasses explicitly?
 
 def main(argv):
     """ RAIL Evaluation module - command line mode:
@@ -14,49 +15,58 @@ def main(argv):
 
     Usage:
     ------
-        python eval.py <PDFs file> <z-spec file>
+        python eval.py <code name> <PDFs file> <sample name> <z-spec file>
 
     Example:
-        python eval.py results/FZBoost/test_FZBoost.hdf5 ../tests/data/test_dc2_validation_9816.hdf5
+        python eval.py FZBoost ./results/FZBoost/test_FZBoost.hdf5 toy_data ../tests/data/test_dc2_validation_9816.hdf5
 
     """
     print()
     print()
     print("        *** RAIL EVALUATION MODULE ***")
     print()
-    if len(argv) != 3:
+    if len(argv) != 5:
         print()
         print("ERROR: invalid entry")
         print()
         print("Usage:")
-        print("    python eval.py <PDFs file> <z-spec file>")
+        print("    python eval.py <code name> <PDFs file> <sample name> <z-spec file>")
         print()
         print("Example:")
-        print("    python eval.py  ./results/FZBoost/test_FZBoost.hdf5 ../tests/data/test_dc2_validation_9816.hdf5")
+        print("    python eval.py FZBoost ./results/FZBoost/test_FZBoost.hdf5 toy_data ../tests/data/test_dc2_validation_9816.hdf5")
         print()
         print()
         sys.exit()
     else:
-        pdfs_file, ztrue_file = argv[1], argv[2]
+        code, pdfs_file, name, ztrue_file = argv[1], argv[2], argv[3], argv[4]
         print()
         print()
-        print(f"Photo-z results (PDFs file): {pdfs_file}")
+        print(f"Photo-z results by: {code}") 
+        print(f"PDFs file: {pdfs_file}")
         print()
-        print(f"Validation/test set (z-true file): {ztrue_file}")
+        print(f"Validation/test set: {name}")
+        print(f"z-true file: {ztrue_file}")
         print()
         print()
 
+    print("Reading data...")
+    print()
+    sample = Sample(pdfs_file, ztrue_file, code ="FZBoost", name="toy data")
+    print(sample)
+    print()
+    print()
 
     print("Computing metrics...")
     print()
     print()
 
 
-    print("Making plots metrics...")
+
+    print("Making plots...")
     print()
     print()
 
-    raise NotImplementedError("Module under construction.")
+    #raise NotImplementedError("Module under construction.")
 
 if __name__ == "__main__":
     main(sys.argv)
