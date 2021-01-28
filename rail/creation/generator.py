f2fd73649fce1e8974617226131d9f37d3bde676
John Franklin Crenshaw <jfc20@uw.edu>
2021-01-27 19:38:36 -0800

    Fixed problem by removed  statements from abstractmethods

--- rail/creation/generator.py
+++ rail/creation/generator.py
@@ -6,19 +6,12 @@ from abc import ABC, abstractmethod
 
 class Generator(ABC):
     def __init__(self):
-
-        return
+        """Initialize Generator that can sample galaxy data and draw redshift posteriors"""
 
     @abstractmethod
     def sample(self, n_samples, seed=None, **kwargs):
-        """
-        Return a random sample of the distribution with size n_samples
-        """
-        pass
+        """Return a random sample of the distribution with size n_samples"""
 
     @abstractmethod
     def pz_estimate(self, data, grid, **kwargs):
-        """
-        Return redshift posteriors for the data
-        """
-        pass
+        """Return redshift posteriors for the data"""
--- tests/creation/test_generator.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from rail.creation import Generator
-
-
-def test_Generator_instantiation():
-    with pytest.raises(TypeError):
-        Generator()
