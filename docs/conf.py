e30dd20a591e4a2fdfd651bf2d0a219a86358c4c
sjoudaki <sjoudaki@gmail.com>
2021-01-30 15:29:18 +0100

    modified rst

--- docs/conf.py
+++ docs/conf.py
@@ -15,11 +15,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
-if on_rtd:
-    html_theme = 'default'
-else:
-    html_theme = 'nature'
 
 # -- Project information -----------------------------------------------------
 
