30aea5f8fb076d92c22cbe137f302d85101fd42f
dagoret <dagoret@lal.in2p3.fr>
2021-02-25 18:30:40 +0100

    in principe do the configfile in the format required by Delight, config file is created locally

--- rail/estimation/algos/delightPZ.py
+++ rail/estimation/algos/delightPZ.py
@@ -8,7 +8,18 @@ import numpy as np
 from scipy.stats import norm
 from rail.estimation.estimator import Estimator as BaseEstimation
 
+import os
+
+import coloredlogs
+import logging
+from pkg_resources import resource_filename
+
 from interfaces.rail.processFilters import processFilters  # interface added into delight in branch rail
+from interfaces.rail.makeConfigParam import makeConfigParam  # build the parameter file required by Delight
+
+# Create a logger object.
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', logger=logger,fmt='%(asctime)s,%(msecs)03d %(programname)s %(name)s[%(process)d] %(levelname)s %(message)s')
 
 
 class delightPZ(BaseEstimation):
@@ -29,15 +40,65 @@ class delightPZ(BaseEstimation):
         self.zmin = inputs['rand_zmin']
         self.zmax = inputs['rand_zmax']
         self.nzbins = inputs['nzbins']
-        self.configfilename= inputs["configfilepath"]
+        self.tempdir = inputs['tempdir']
+        self.tempdatadir = inputs['tempdatadir']
+        self.delightparamfile=inputs["delightparamfile"]
+        self.delightparamfile = os.path.join(self.tempdir, self.delightparamfile)
+        self.inputs=inputs
+
         np.random.seed(87)
+
+    #############################################################################################################
+    #
+    # INFORM
+    #
+    ############################################################################################################
 
     def inform(self):
         """
           this is random, so does nothing
         """
-        print("Try to workout filters")
-        processFilters(self.configfilename)
+        logger.info("Try to workout filters")
+
+        # create usefull tempory directory
+        try:
+            os.makedirs(self.tempdir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        try:
+            os.makedirs(self.tempdatadir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+
+
+
+        # Very awfull way to get the internal data path for Delight
+        # This is the delight setup.py tools that get the data there
+        basedelight_datapath = resource_filename('delight', '../data')
+        logger.debug("Guessed basedelight_datapath  = " + basedelight_datapath )
+
+        # gives to Delight where are its datapath and provide yaml arguments
+        paramfile_txt=makeConfigParam(basedelight_datapath, self.inputs)
+        logger.debug(paramfile_txt)
+
+
+        # save the config  parameter file that Delight needs
+        with open(self.delightparamfile, 'w') as out:
+            out.write(paramfile_txt)
+
+        # Build LSST filter model
+        processFilters(self.delightparamfile)
+
+    #############################################################################################################
+    #
+    # ESTIMATE
+    #
+    ############################################################################################################
+
 
     def estimate(self, test_data):
         pdf = []
