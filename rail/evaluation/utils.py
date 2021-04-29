81ff354dc1a353a86c65be26a30bc5eca1929b11
Julia Gschwend <julia@linea.gov.br>
2021-04-28 22:37:40 -0300

    removing class sample

--- rail/evaluation/utils.py
+++ rail/evaluation/utils.py
@@ -29,7 +29,7 @@ def read_pz_output(pdfs_file, ztrue_file, pdfs_key="photoz_pdf", zgrid_key="zgri
             zgrid = np.array(pf[zgrid_key]).flatten()
             photoz_mode = np.array(pf[photoz_mode_key])
     elif ext == ".out":
-        print("Validation file from DC1 paper!")
+        print("Validation file from DC1 paper (ascii format).")
         ztrue = np.loadtxt(ztrue_file, unpack=True, usecols=[2])
         pdfs = np.loadtxt(pdfs_file)
         path = "/".join(pdfs_file.split("/")[:-1])
@@ -77,7 +77,7 @@ def plot_pdfs(sample, gals, show_ztrue=True, show_photoz_mode=False):
     return colors
 
 
-def plot_old_valid(sample, gals=None, colors=None):
+def plot_old_valid(photoz, ztrue, gals=None, colors=None, code=""):
     """Plot traditional Zphot X Zspec and N(z) plots for illustration
     Ancillary function to be used by class Sample.
 
@@ -88,13 +88,13 @@ def plot_old_valid(sample, gals=None, colors=None):
     colors: `list`, (optional)
         list of HTML codes for colors used in the plot highlighted points
     """
-    df = pd.DataFrame({'z$_{true}$': sample.ztrue,
-                       'z$_{phot}$': sample.photoz_mode})
+    df = pd.DataFrame({'z$_{true}$': ztrue,
+                       'z$_{phot}$': photoz})
     fig = plt.figure(figsize=(10, 4), dpi=100)
-    fig.suptitle(sample.name, fontsize=16)
+    #fig.suptitle(name, fontsize=16)
     ax = plt.subplot(121)
     # sns.jointplot(data=df, x='z$_{true}$', y='z$_{phot}$', kind="kde")
-    plt.plot(sample.ztrue, sample.photoz_mode, 'k,', label=(sample._code).replace("_", " "))
+    plt.plot(ztrue, photoz, 'k,', label=code)
     leg = ax.legend(fancybox=True, handlelength=0, handletextpad=0, loc="upper left")
     for item in leg.legendHandles:
         item.set_visible(False)
@@ -102,23 +102,23 @@ def plot_old_valid(sample, gals=None, colors=None):
         if not colors:
             colors = ['r'] * len(gals)
         for i, gal in enumerate(gals):
-            plt.plot(sample.ztrue[gal], sample.photoz_mode[gal], 'o', color=colors[i], label=f'Galaxy {gal}')
-    zmax = np.max(sample.ztrue) * 1.01
+            plt.plot(ztrue[gal], photoz[gal], 'o', color=colors[i], label=f'Galaxy {gal}')
+    zmax = np.max(ztrue) * 1.01
     plt.xlim(0, zmax)
     plt.ylim(0, zmax)
     plt.xlabel('z$_{true}$')
     plt.ylabel('z$_{phot}$')
     plt.subplot(122)
-    sns.kdeplot(sample.ztrue, shade=True, label='z$_{true}$')
-    sns.kdeplot(sample.photoz_mode, shade=True, label='z$_{phot}$')
+    sns.kdeplot(ztrue, shade=True, label='z$_{true}$')
+    sns.kdeplot(photoz, shade=True, label='z$_{phot}$')
     plt.xlim(0, zmax)
     plt.xlabel('z')
     plt.legend()
     plt.tight_layout()
 
 
-def old_metrics(sample):
-    point = EvaluatePointStats(sample.photoz_mode, sample.ztrue)
+def old_metrics(photoz, ztrue):
+    point = EvaluatePointStats(photoz, ztrue)
     sigma_iqr = point.CalculateSigmaIQR()
     bias = point.CalculateBias()
     frac = point.CalculateOutlierRate()
@@ -126,17 +126,14 @@ def old_metrics(sample):
     return sigma_iqr, bias, frac, sigma_mad
 
 
-def old_metrics_table(samples, show_dc1=False):
-    if type(samples) != list:
-        samples = [samples]
+def old_metrics_table(photoz, ztrue, name="", show_dc1=True):
     rows = ["|Metric |", "|:---|", "|scatter |", "|bias |", "|outlier rate |"]
-    for sample in samples:
-        sigma_iqr, bias, frac, sigma_mad = old_metrics(sample)
-        rows[0] += f"{sample.code} {sample.name} |"
-        rows[1] += "---:|"
-        rows[2] += f"{sigma_iqr:11.4f} |"
-        rows[3] += f"{bias:11.5f} |"
-        rows[4] += f"{frac:11.3f} |"
+    sigma_iqr, bias, frac, sigma_mad = old_metrics(photoz, ztrue)
+    rows[0] += f"{name} |"
+    rows[1] += "---:|"
+    rows[2] += f"{sigma_iqr:11.4f} |"
+    rows[3] += f"{bias:11.5f} |"
+    rows[4] += f"{frac:11.3f} |"
     if show_dc1:
         rows[0] += "DC1 paper"
         rows[1] += "---:"
@@ -400,3 +397,118 @@ class DC1:
             table += f" | {self.results['AD'][code]:11.4f}"
             table += f" | {self.results['CDE loss'][code]:11.4f}\n"
         return Markdown(table)
+
+
+class Sample(Ensemble):
+    """ Expand qp.Ensemble to append true redshifts
+    array, metadata, and specific plots. """
+
+    def __init__(self, pdfs, zgrid, ztrue, photoz_mode, code="", name="", n_quant=100):
+        """Class constructor
+
+        Parameters
+        ----------
+        pdfs: `ndarray`
+            photo-z PDFs array, shape=(Ngals, Nbins)
+        zgrid: `ndarray`
+            PDF bins centers, shape=(Nbins,)
+        ztrue: `ndarray`
+            true redshifts, shape=(Ngals,)
+        photoz_mode: `ndarray`
+            photo-z (PDF mode), shape=(Ngals,)
+        code: `str`, (optional)
+            algorithm name (for plot legends)
+        name: `str`, (optional)
+            sample name (for plot legends)
+        """
+
+        super().__init__(interp, data=dict(xvals=zgrid, yvals=pdfs))
+        self._pdfs = pdfs
+        self._zgrid = zgrid
+        self._ztrue = ztrue
+        self._photoz_mode = photoz_mode
+        self._code = code
+        self._name = name
+        self._n_quant = n_quant
+        self._pit = None
+        self._qq = None
+
+
+    @property
+    def code(self):
+        """Photo-z code/algorithm name"""
+        return self._code
+
+    @property
+    def name(self):
+        """Sample name"""
+        return self._name
+
+    @property
+    def ztrue(self):
+        """True redshifts array"""
+        return self._ztrue
+
+    @property
+    def zgrid(self):
+        """Redshift grid (binning)"""
+        return self._zgrid
+
+    @property
+    def photoz_mode(self):
+        """Photo-z (mode) array"""
+        return self._photoz_mode
+
+    @property
+    def n_quant(self):
+        return self._n_quant
+
+    @property
+    def pit(self):
+        if self._pit is None:
+            pit_array = np.array([self[i].cdf(self.ztrue[i])[0][0] for i in range(len(self))])
+            self._pit = pit_array
+        return self._pit
+
+    @property
+    def qq(self, n_quant=100):
+        q_theory = np.linspace(0., 1., n_quant)
+        q_data = np.quantile(self.pit, q_theory)
+        self._qq = (q_theory, q_data)
+        return self._qq
+
+    def __len__(self):
+        if len(self._ztrue) != len(self._pdfs):
+            raise ValueError("Number of pdfs and true redshifts do not match!!!")
+        return len(self._ztrue)
+
+    def __str__(self):
+        code_str = f'Algorithm: {self._code}'
+        name_str = f'Sample: {self._name}'
+        line_str = '-' * (max(len(code_str), len(name_str)))
+        text = str(line_str + '\n' +
+                   name_str + '\n' +
+                   code_str + '\n' +
+                   line_str + '\n' +
+                   f'{len(self)} PDFs with {len(self.zgrid)} probabilities each \n' +
+                   f'qp representation: {self.gen_class.name} \n' +
+                   f'z grid: {len(self.zgrid)} z values from {np.min(self.zgrid)} to {np.max(self.zgrid)} inclusive')
+        return text
+
+    def plot_pdfs(self, gals, show_ztrue=True, show_photoz_mode=False):
+        colors = utils.plot_pdfs(self, gals, show_ztrue=show_ztrue,
+                                 show_photoz_mode=show_photoz_mode)
+        return colors
+
+    def plot_old_valid(self, gals=None, colors=None):
+        old_metrics_table = utils.plot_old_valid(self, gals=gals, colors=colors)
+        return old_metrics_table
+
+    def plot_pit_qq(self, bins=None, label=None, title=None, show_pit=True,
+                    show_qq=True, show_pit_out_rate=True, savefig=False):
+        """Make plot PIT-QQ as Figure 2 from Schmidt et al. 2020."""
+        fig_filename = utils.plot_pit_qq(self, bins=bins, label=label, title=title,
+                                         show_pit=show_pit, show_qq=show_qq,
+                                         show_pit_out_rate=show_pit_out_rate,
+                                         savefig=savefig)
+        return fig_filename
