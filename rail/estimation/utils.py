4da15be2c7fba559446877d2edddc29fdc8735aa
sschmidt23 <sschmidt@physics.ucdavis.edu>
2021-05-04 16:45:21 -0700

    forgot to include new utils.py

---
+++ rail/estimation/utils.py
@@ -0,0 +1,24 @@
+def check_and_print_params(config_dict, default_param_dict, desc_dict):
+    """
+    Function that tests that all required parameters are in file, if
+    not, then add default value.  It also prints a short description
+    of each parameter along with the value being used.
+    Inputs:
+    -------
+    config_dict: dict
+      dictionary of input params
+    Output:
+    -------
+    updated_dict: dict
+      updated parameter dictionary
+    """
+    print("summary of trainZ inputs:")
+    params_dict = default_param_dict['run_params']
+    for key in params_dict.keys():
+        if key not in config_dict['run_params'].keys():
+            print(f"param {key} not included in config, using default value")
+            config_dict['run_params'][key] = params_dict[key]
+        print(f"{key}: {desc_dict[key]}")
+        print(f"{key} value: {config_dict['run_params'][key]}\n")
+    return config_dict
+
