f81e25805714011d78478663fd46abdcfb66fab0
sschmidt23 <sschmidt@physics.ucdavis.edu>
2021-05-04 16:51:49 -0700

    fix extra \n and typo in utils

--- rail/estimation/utils.py
+++ rail/estimation/utils.py
@@ -12,13 +12,13 @@ def check_and_print_params(config_dict, default_param_dict, desc_dict):
     updated_dict: dict
       updated parameter dictionary
     """
-    print("summary of trainZ inputs:")
+    print("summary of input parameters:")
     params_dict = default_param_dict['run_params']
     for key in params_dict.keys():
         if key not in config_dict['run_params'].keys():
             print(f"param {key} not included in config, using default value")
             config_dict['run_params'][key] = params_dict[key]
         print(f"{key}: {desc_dict[key]}")
-        print(f"{key} value: {config_dict['run_params'][key]}\n")
+        print(f"{key} value: {config_dict['run_params'][key]}")
     return config_dict
 
