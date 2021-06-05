bd422de64dd43288869a682517ea9af402a98f49
sjoudaki <sjoudaki@gmail.com>
2021-06-05 21:00:44 +0200

    minor update

--- docs/conf.py
+++ docs/conf.py
@@ -73,8 +73,8 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'setup.rst', 'source/index_body.rst']
-# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'setup.rst']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'setup.rst',
+                    'source/index_body.rst', 'api/rail.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
