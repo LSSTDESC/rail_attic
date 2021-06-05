a51bf996f61a89a3e0176033aab7c3084a415e53
sjoudaki <sjoudaki@gmail.com>
2021-06-05 19:12:28 +0200

    testing

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# apidoc_module_dir = '../rail/docs'
+apidoc_module_dir = '../rail'
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -75,8 +75,8 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'setup.rst', 'api/rail.rst']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'setup.rst']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'setup.rst', 'api/rail.rst']
+# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'setup.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
