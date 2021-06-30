718db59eddc382b38ffb60173e9d28ab36c9db28
Julia Gschwend <julia@linea.gov.br>
2021-06-30 16:24:52 -0300

    Refactor CDE Loss metric to receive qp.Ensemble as input and to return output as standardized named tuple as all other metrics.

--- rail/evaluation/metrics/cdeloss.py
+++ rail/evaluation/metrics/cdeloss.py
@@ -3,17 +3,21 @@ import numpy as np
 from scipy import stats
 import qp
 from rail.evaluation.evaluator import Evaluator
+from rail.evaluation.utils import stat_and_pval, stat_crit_sig
+
 
 default_grid = np.linspace(0., 1., 100)
 
 class CDELoss(Evaluator):
     """ Conditional density loss """
-    def __init__(self, qp_ens, ztrue):
+    def __init__(self, qp_ens, zgrid, ztrue):
         """Class constructor"""
         super().__init__(qp_ens)
 
+        self._pdfs = qp_ens.pdf(zgrid)
+        self._xvals = zgrid
         self._ztrue = ztrue
-        self._pdfs = qp_ens
+
 
     def evaluate(self, eval_grid=default_grid):
         """Evaluate the estimated conditional density loss described in
@@ -24,16 +28,11 @@ class CDELoss(Evaluator):
         Double-check that this is doing the right thing and that the types work out. . .
         """
 
-        # first step here needs to be conversion to gridded parameterization
-        grid_pdfs = self._pdfs.convert_to(qp.interp_gen, xvals=eval_grid)
-        yvals = grid_pdfs.pdf(eval_grid)
-        #  (or some simple transformation thereof?)
-
         # Calculate first term E[\int f*(z | X)^2 dz]
-        term1 = np.mean(np.trapz(ygrid ** 2, x=self._xvals))
+        term1 = np.mean(np.trapz(self._pdfs ** 2, x=self._xvals))
         # z bin closest to ztrue
         nns = [np.argmin(np.abs(eval_grid - z)) for z in self._ztrue]
         # Calculate second term E[f*(Z | X)]
-        term2 = np.mean(pdf[:, nns])
+        term2 = np.mean(self._pdfs[:, nns])
         cdeloss = term1 - 2 * term2
-        return cdeloss
+        return stat_and_pval(cdeloss, None)
