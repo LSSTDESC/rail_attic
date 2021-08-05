422b1da3a395d0d115416e836ee03563684e0bce
sschmidt23 <sschmidt@physics.ucdavis.edu>
2021-08-05 16:14:22 -0700

    fix bugs in cdeloss

--- rail/evaluation/metrics/cdeloss.py
+++ rail/evaluation/metrics/cdeloss.py
@@ -17,9 +17,9 @@ class CDELoss(Evaluator):
         self._pdfs = qp_ens.pdf(zgrid)
         self._xvals = zgrid
         self._ztrue = ztrue
+        self._npdf = qp_ens.npdf
 
-
-    def evaluate(self, eval_grid=default_grid):
+    def evaluate(self):
         """Evaluate the estimated conditional density loss described in
         Izbicki & Lee 2017 (arXiv:1704.08095).
 
@@ -31,8 +31,8 @@ class CDELoss(Evaluator):
         # Calculate first term E[\int f*(z | X)^2 dz]
         term1 = np.mean(np.trapz(self._pdfs ** 2, x=self._xvals))
         # z bin closest to ztrue
-        nns = [np.argmin(np.abs(eval_grid - z)) for z in self._ztrue]
+        nns = [np.argmin(np.abs(self._xvals - z)) for z in self._ztrue]
         # Calculate second term E[f*(Z | X)]
-        term2 = np.mean(self._pdfs[:, nns])
+        term2 = np.mean(self._pdfs[range(self._npdf), nns])
         cdeloss = term1 - 2 * term2
         return stat_and_pval(cdeloss, None)
