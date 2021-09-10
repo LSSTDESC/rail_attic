016e9b504d6344a3a02a6ad19465bc33621418ce
sschmidt23 <sschmidt@physics.ucdavis.edu>
2021-09-09 21:37:29 -0700

    remove old tests

--- tests/test_fileIO.py
+++ tests/test_fileIO.py
@@ -2,28 +2,13 @@ import pytest
 import qp
 import os
 import numpy as np
-from rail.fileIO import load_training_data, initialize_qp_output
+from rail.fileIO import initialize_qp_output
 from rail.fileIO import write_qp_output_chunk, qp_reformat_output
-from rail.fileIO import initialize_writeout
 
 
 h5_data_file = 'tests/data/pandas_test_hdf5.h5'
 parquet_data_file = 'tests/data/parquet_test.parquet'
 no_group_file = 'tests/data/no_groupname_test.hdf5'
-
-
-def test_pandas_readin():
-    _ = load_training_data(h5_data_file, fmt='h5')
-    _ = load_training_data(parquet_data_file, fmt='parquet')
-
-
-def test_no_groupname():
-    _ = load_training_data(no_group_file, fmt='hdf5', groupname='None')
-
-
-def test_missing_file_ext():
-    with pytest.raises(NotImplementedError):
-        _ = load_training_data(no_group_file, fmt='csv')
 
 
 def test_qp_file_writing():
