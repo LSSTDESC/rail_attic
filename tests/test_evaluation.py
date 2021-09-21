0e44c6a7b6c7f2d94ec4f40571ad07eb0faba7a5
sschmidt23 <sschmidt@physics.ucdavis.edu>
2021-09-20 18:10:17 -0700

    add point est to evaluation

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,25 +1,31 @@
 import numpy as np
 from rail.evaluation.metrics.pit import PIT, PITOutRate, PITKS, PITCvM, PITAD
 from rail.evaluation.metrics.cdeloss import CDELoss
+import rail.evaluation.metrics.pointestimates as pe
 import qp
 
-# values for metrics
-OUTRATE = 0.5900099
-KSVAL = 0.5964912
-CVMVAL = 42.94254
-ADVAL_ALL = 224.416922
-ADVAL_CUT = -0.655614
-CDEVAL = -45.9017221
+# metric values for specific test ensemble
+OUTRATE = 0.0
+KSVAL = 0.367384
+CVMVAL = 20.63155
+ADVAL_ALL = 82.51480
+ADVAL_CUT = 1.10750
+CDEVAL = -4.31200
+SIGIQR = 0.0045947
+BIAS = -0.00001576
+OUTRATE = 0.0
+SIGMAD = 0.0046489
 
 
 def construct_test_ensemble():
     np.random.seed(87)
+    nmax = 2.5
     NPDF = 399
-    true_zs = np.random.uniform(high=2.5, size=NPDF)
+    true_zs = np.random.uniform(high=nmax, size=NPDF)
     locs = np.expand_dims(true_zs + np.random.normal(0.0, 0.01, NPDF), -1)
     scales = np.ones((NPDF, 1)) * 0.1 + np.random.uniform(size=(NPDF, 1)) * .05
     n_ens = qp.Ensemble(qp.stats.norm, data=dict(loc=locs, scale=scales))
-    zgrid = np.linspace(0, 1, 301)
+    zgrid = np.linspace(0, nmax, 301)
     grid_ens = n_ens.convert_to(qp.interp_gen, xvals=zgrid)
     return zgrid, true_zs, grid_ens
 
@@ -50,3 +56,20 @@ def test_pit_metrics():
     cde_obj = CDELoss(pdf_ens, zgrid, zspec)
     cde_stat = cde_obj.evaluate().statistic
     assert np.isclose(cde_stat, CDEVAL)
+
+
+def test_point_metrics():
+    zgrid, zspec, pdf_ens = construct_test_ensemble()
+    zb = pdf_ens.mode(grid=zgrid).flatten()
+
+    sig_iqr = pe.PointSigmaIQR(zb, zspec).evaluate()
+    assert np.isclose(sig_iqr, SIGIQR)
+
+    bias = pe.PointBias(zb, zspec).evaluate()
+    assert np.isclose(bias, BIAS)
+
+    out_rate = pe.PointOutlierRate(zb, zspec).evaluate()
+    assert np.isclose(out_rate, OUTRATE)
+
+    sig_mad = pe.PointSigmaMAD(zb, zspec).evaluate()
+    assert np.isclose(sig_mad, SIGMAD)
