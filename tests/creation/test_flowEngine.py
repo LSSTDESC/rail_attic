26bf9e8b56d0534ef0ba59da62c2dca209141041
John Franklin Crenshaw <jfc20@uw.edu>
2021-09-24 14:08:00 -0700

    Updated example function names from pzflow.

--- tests/creation/test_flowEngine.py
+++ tests/creation/test_flowEngine.py
@@ -1,10 +1,10 @@
 import numpy as np
-from pzflow.examples import example_flow, galaxy_data
+from pzflow.examples import get_example_flow, get_galaxy_data
 from rail.creation.engines import FlowEngine
 
 
 def test_flowengine_sample():
-    flow = example_flow()
+    flow = get_example_flow()
     flowEng = FlowEngine(flow)
     assert np.allclose(
         flow.sample(10, seed=0).values, flowEng.sample(10, seed=0).values
@@ -12,9 +12,9 @@ def test_flowengine_sample():
 
 
 def test_flowengine_pz_estimate():
-    flow = example_flow()
+    flow = get_example_flow()
     flowEng = FlowEngine(flow)
-    data = galaxy_data()[:10]
+    data = get_galaxy_data()[:10]
     grid = np.arange(0, 2.5, 0.5)
     assert np.allclose(
         flow.posterior(data, column="redshift", grid=grid),
