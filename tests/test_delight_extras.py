9bb29f6bbe37b24eeac29933c4d448e567cdbe2a
sschmidt23 <sschmidt@physics.ucdavis.edu>
2022-01-14 16:13:55 -0800

    fix path

--- tests/test_delight_extras.py
+++ tests/test_delight_extras.py
@@ -11,5 +11,5 @@ def test_delight_default_file_gen():
     test_file = "test_delight_config.txt"
     with open(test_file, "w") as f:
         f.write(config)
-    assert filecmp.cmp(test_file, "./data/delight_default_param.txt")
+    assert filecmp.cmp(test_file, "./tests/data/delight_default_param.txt")
     os.remove(test_file)
