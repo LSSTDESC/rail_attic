9335ce2441b760810a494474339b4731efee07b3
Eric Charles <echarles@gmail.com>
2022-03-13 18:56:15 -0400

    Added working version of goldenspike notebook and cleanup script

--- examples/goldenspike/cleanup.sh
+++ examples/goldenspike/cleanup.sh
@@ -9,8 +9,8 @@ if [[ -z "${RAILDIR}" ]] then;
    \rm -rf examples/goldenspike/evaluation_results
    \rm -rf examples/goldenspike/*.out
    \rm -rf examples/goldenspike/data/pretrained_flow_copy.pkl
-   \rm -rf examples/goldenspike/goldenspike.yml
-   \rm -rf examples/goldenspike/goldenspike_config.yml
+   \rm -rf examples/goldenspike/tmp_goldenspike.yml
+   \rm -rf examples/goldenspike/tmp_goldenspike_config.yml
 else
    \rm -rf ${RAILDIR}/examples/goldenspike/output_*.pq
    \rm -rf ${RAILDIR}/examples/goldenspike/output_*.fits
@@ -21,7 +21,7 @@ else
    \rm -rf ${RAILDIR}/examples/goldenspike/evaluation_results
    \rm -rf ${RAILDIR}/examples/goldenspike/*.out
    \rm -rf ${RAILDIR}/examples/goldenspike/data/pretrained_flow_copy.pkl
-   \rm -rf ${RAILDIR}/examples/goldenspike/goldenspike.yml
-   \rm -rf ${RAILDIR}/examples/goldenspike/goldenspike_config.yml     
+   \rm -rf ${RAILDIR}/examples/goldenspike/tmp_goldenspike.yml
+   \rm -rf ${RAILDIR}/examples/goldenspike/tmp_goldenspike_config.yml     
 fi
 
