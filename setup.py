46eab164fc37a3df6f15973a68b3c00e7d7dc615
sschmidt23 <sschmidt@physics.ucdavis.edu>
2022-03-15 14:48:54 -0700

    update setup.py to separate delight, consistent install for readme and docs

--- setup.py
+++ setup.py
@@ -26,14 +26,17 @@ estimation_codes = {
     "bpz": ["DESC_BPZ @ git+https://github.com/LSSTDESC/DESC_BPZ"],
     "flex": ["FlexCode[all]"],
     "NN": ["sklearn"],
-    "delightPZ": [
-        "coloredlogs",
-        "corner",
-        "cython",
-        "emcee",
-        "delight @ git+https://github.com/LSSTDESC/Delight",
-        ]
 }
+# dependencies for Delight, separate out because it can be a
+# pain to install on Mac due to dropped default openmp
+delight_extras = [
+    "coloredlogs",
+    "corner",
+    "cython",
+    "emcee",
+    "delight @ git+https://github.com/LSSTDESC/Delight",
+]
+
 
 # dependencies for the Evaluation module
 evaluation_extras = ["seaborn"]
@@ -45,10 +48,11 @@ extras_require["creation"] = creation_extras
 extras_require["estimation"] = estimation_extras + list(
     set(sum(estimation_codes.values(), []))
 )
+extras_require["delight"] = delight_extras
 for key, values in estimation_codes.items():
     extras_require[key] = estimation_extras + values
 extras_require["evaluation"] = evaluation_extras
-extras_require["all"] = extras_require["full"] = extras_require["Full"] = list(
+extras_require["base"] = list(
     set(
         (
             extras_require["creation"]
@@ -58,6 +62,15 @@ extras_require["all"] = extras_require["full"] = extras_require["Full"] = list(
     )
 )
 
+extras_require["all"] = extras_require["full"] = extras_require["Full"] = list(
+    set(
+        (
+            extras_require["base"]
+            + extras_require["delight"]
+        )
+    )
+)
+        
 # load the version number
 with open("rail/version.py") as f:
     __version__ = f.read().replace('"', "").split("=")[1]
@@ -69,7 +82,7 @@ setup(
     author="The LSST DESC PZ WG",
     author_email="aimalz@nyu.edu",
     packages=find_namespace_packages(),
-    package_dir={'rail': './rail','rail.estimation':'./rail/estimation','rail.estimation.algos': './rail/estimation/algos'},
+    package_dir={'rail': './rail', 'rail.estimation':'./rail/estimation', 'rail.estimation.algos': './rail/estimation/algos'},
     package_data={
         "": ["*.hdf5", "*.yaml", "*.sed", "*.res", "*.AB", "*.list", "*.columns"],
         "tests": ["*.hdf5", "*.yaml", "*.columns"],
