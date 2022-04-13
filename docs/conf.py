237375a8e70ba080bd1e7fa8077a96a9f599f047
Eric Charles <echarles@gmail.com>
2022-04-13 15:33:17 -0700

    more docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -139,7 +139,7 @@ htmlhelp_basename = 'raildoc'
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_elements = {
+#latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
@@ -155,25 +155,25 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
-}
+#}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
+#latex_documents = [
     #(master_doc, 'rail.tex', 'RAIL Documentation',
     # 'railteam', 'Manual'),
-]
+#]
 
 
 # -- Options for manual page output ------------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
+#man_pages = [
     #(master_doc, 'RAIL', 'RAIL Documentation',
     # [author], 1)
-]
+#]
 
 
 # -- Options for Texinfo output ----------------------------------------------
@@ -181,11 +181,11 @@ man_pages = [
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [
+#texinfo_documents = [
     #(master_doc, 'RAIL', 'RAIL Documentation',
     # author, 'RAIL', 'One line description of project.',
     # 'Miscellaneous'),
-]
+#]
 
 
 # -- Extension configuration -------------------------------------------------
