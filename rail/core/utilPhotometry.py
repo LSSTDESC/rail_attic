5a79d500fb65d6faa56c0f3aaaf87efa98c1685c
jlvdb <jlvdb@astro.rub.de>
2022-05-03 15:25:12 +0200

    added superclass and missing docstrings, refactoring

--- rail/core/utilPhotometry.py
+++ rail/core/utilPhotometry.py
@@ -1,41 +1,83 @@
+"""
+Module that implements operations on photometric data such as magnitudes and fluxes.
+"""
+from abc import ABC, abstractmethod
+
 import numpy as np
 import pandas as pd
 
-import tables_io
 from ceci.config import StageParameter as Param
 from rail.core.data import PqHandle
 from rail.core.stage import RailStage
 
-# external code
-import hyperbolic
+import hyperbolic  # https://github.com/jlvdb/hyperbolic
 
 
-def_bands = ['u', 'g', 'r', 'i', 'z', 'y']
-mag_cols = [f"mag_{band}_lsst" for band in def_bands]
-magerr_cols = [f"mag_err_{band}_lsst" for band in def_bands]
+# default column names in DC2
+lsst_bands = 'ugrizy'
+mag_cols = [f"mag_{band}_lsst" for band in lsst_bands]
+magerr_cols = [f"mag_err_{band}_lsst" for band in lsst_bands]
 
 
-class HyperbolicSmoothing(RailStage):
+def _compute_flux(mags, zeropoint):
     """
-    TODO...
+    Compute the flux corresponding to a given magnitude and photometric zeropoint.
+
+    Parameters
+    ----------
+    mags : array-like
+        Magnitude or array of magnitudes.
+    zeropoint : `float`
+        Photometric zeropoint used for conversion.
+
+    Returns
+    -------
+    fluxes : array-like
+        Flux values.
+    """
+    fluxes = np.exp((zeropoint - mags) / hyperbolic.pogson)
+    return fluxes
+
+
+def _compute_flux_error(fluxes, magerrs):
+    """
+    Compute the flux error corresponding to a given flux and magnitude error.
+
+    Parameters
+    ----------
+    fluxes : array-like
+        Flux or array of flux values.
+    magerrs : array-like
+        Magnitude error or array of magnitude errors.
+
+    Returns
+    -------
+    flux_errors : array-like
+        Flux error values.
+    """
+    flux_errors = magerrs / hyperbolic.pogson * fluxes
+    return flux_errors
+
+
+class MagnitudeManipulator(RailStage, ABC):
+    """
+    Base class to perform opertations on magnitudes. A table with input magnitudes and errors is
+    processed and transformed into an output table with magnitudes and errors.
+
+    Subclasses must implement the run() and compute() method.
     """
 
-    name = 'HyperbolicSmoothing'
+    name = 'MagnitudeManipulator'
     config_options = RailStage.config_options.copy()
     config_options.update(
-        zeropoint=Param(
-            float, 0.0,
-            msg="magnitude zeropoint"),
         magnitude_columns=Param(
             list, mag_cols,
             msg="magnitudes for which hyperbolic magnitudes are computed"),
         magerror_columns=Param(
             list, magerr_cols,
-            msg="magnitude errors corresponding to magnitude_columns "
-                "(assuming same ordering)")
-    )
+            msg="magnitude errors corresponding to magnitude_columns (assuming same ordering)"))
     inputs = [('input', PqHandle)]
-    outputs = [('parameters', PqHandle)]
+    outputs = [('output', PqHandle)]
 
     def __init__(self, args, comm=None):
         super().__init__(args, comm)
@@ -43,28 +85,76 @@ class HyperbolicSmoothing(RailStage):
         self.error_names = self.config.magerror_columns
         if (n_mag := len(self.mag_names)) != (n_err := len(self.error_names)):
             raise ValueError(
-                "number of magnitude and magnitude error columns do not match "
-                f"({n_mag} != {n_err})")
-        self.ZP = self.config.zeropoint
+                f"number of magnitude and error columns do not match ({n_mag} != {n_err})")
+
+    @abstractmethod
+    def run(self):
+        """
+        Implements the operation performed on the input data.
+        """
+        data = self.get_data('input', allow_missing=True)
+        self.add_data('parameters', data)
+
+    @abstractmethod
+    def compute(self, data):
+        """
+        Main method to call.
+
+        Parameters
+        ----------
+        data : `PqHandle`
+           Input tabular data with column names as defined in the configuration.
+
+        Returns
+        -------
+        output: `PqHandle`
+            Output tabular data.
+        """
+        self.set_data('input', data)
+        self.run()
+        self.finalize()
+        return self.get_handle('output')
+
+
+class HyperbolicSmoothing(MagnitudeManipulator):
+    """
+    Initial stage to compute hyperbolic magnitudes (Lupton et al. 1999). Estimates the smoothing
+    parameter b that is used by the second stage (`HyperbolicMagnitudes`) to convert classical to
+    hyperbolic magnitudes.
+    """
+
+    name = 'HyperbolicSmoothing'
+    config_options = MagnitudeManipulator.config_options.copy()
+    config_options.update(
+        zeropoint=Param(
+            float, 0.0,
+            msg="magnitude zeropoint"))
+    inputs = [('input', PqHandle)]
+    outputs = [('parameters', PqHandle)]
+
+    def __init__(self, args, comm=None):
+        super().__init__(args, comm)
+        self.zeropoint = self.config.zeropoint
 
     def run(self):
+        """
+        Computes the smoothing parameter b (see Lupton et al. 1999) per photometric band.
+        """
         # get input data
         data = self.get_data('input', allow_missing=True)
         fields = np.zeros(len(data), dtype=np.int)  # placeholder
 
-        # compute the optimal smoothing factor b for each filter
+        # compute the optimal smoothing factor b for each photometric band
         stats = []
         for mag_col, magerr_col in zip(self.mag_names, self.error_names):
 
             # compute fluxes from magnitudes
-            mags = data[mag_col]
-            magerrs = data[magerr_col]
-            fluxes = np.exp((self.ZP - mags) / hyperbolic.pogson)
-            fluxerrs = magerrs / hyperbolic.pogson * fluxes
+            fluxes = _compute_flux(data[mag_col], self.zeropoint)
+            fluxerrs = _compute_flux_error(fluxes, data[magerr_col])
 
             # compute the median flux error and zeropoint
             stats_filt = hyperbolic.compute_flux_stats(
-                fluxes, fluxerrs, fields, zeropoint=self.ZP)
+                fluxes, fluxerrs, fields, zeropoint=self.zeropoint)
             # compute the smoothing parameter b (in normalised flux)
             stats_filt[hyperbolic.Keys.b] = hyperbolic.estimate_b(
                 stats_filt[hyperbolic.Keys.zp],
@@ -82,11 +172,23 @@ class HyperbolicSmoothing(RailStage):
             stats.append(stats_filt)
 
         # store resulting smoothing parameters for next stage
-        stats = pd.concat(stats)
-        self.add_data('parameters', stats)
+        self.add_data('parameters', pd.concat(stats))
 
     def compute(self, data):
-        """...
+        """
+        Main method to call. Computes the set of smoothing parameters for an input catalogue with
+        classical magitudes and their respective errors. These parameters are required by the
+        follow-up stage `HyperbolicMagnitudes`.
+
+        Parameters
+        ----------
+        data : `PqHandle`
+            Input table with magnitude and magnitude error columns as defined in the configuration.
+
+        Returns
+        -------
+        parameters : `PqHandle`
+            Table with smoothing parameters per photometric band and additional meta data.
         """
         self.set_data('input', data)
         self.run()
@@ -94,39 +196,48 @@ class HyperbolicSmoothing(RailStage):
         return self.get_handle('parameters')
 
 
-class HyperbolicMagnitudes(RailStage):
+class HyperbolicMagnitudes(MagnitudeManipulator):
     """
-    TODO...
+    Convert a set of classical magnitudes to hyperbolic magnitudes  (Lupton et al. 1999). Requires
+    input from the initial stage (`HyperbolicSmoothing`) to supply optimal values for the smoothing
+    parameter.
     """
 
     name = 'HyperbolicMagnitudes'
-    config_options = RailStage.config_options.copy()
-    config_options.update(
-        magnitude_columns=Param(
-            list, mag_cols,
-            msg="magnitudes for which hyperbolic magnitudes are computed"),
-        magerror_columns=Param(
-            list, magerr_cols,
-            msg="magnitude errors corresponding to magnitude_columns "
-                "(assuming same ordering)")
-    )
+    config_options = MagnitudeManipulator.config_options.copy()
     inputs = [('input', PqHandle),
               ('parameters', PqHandle)]
     outputs = [('output', PqHandle)]
 
-    def __init__(self, args, comm=None):
-        super().__init__(args, comm)
-        self.mag_names = self.config.magnitude_columns
-        self.error_names = self.config.magerror_columns
-        if (n_mag := len(self.mag_names)) != (n_err := len(self.error_names)):
-            raise ValueError(
-                "number of magnitude and magnitude error columns do not match "
-                f"({n_mag} != {n_err})")
+    def _check_filters(self, stats_table):
+        """
+        Check whether the column definition matche the loaded smoothing parameters.
+
+        Parameters:
+        -----------
+        stats_table : `pd.DataFrame`
+            Data table that contains smoothing parameters per photometric band (from
+            `HyperbolicSmoothing`).
+
+        Raises
+        ------
+        ValueError : Filter defined in magnitude_columns is not found in smoothing parameter table.
+        """
+        param_filters = set(stats_table.reset_index()[hyperbolic.Keys.filter])
+        config_filters = set(self.mag_names)
+        filter_diff = config_filters - param_filters
+        if len(filter_diff) != 0:
+            strdiff = ", ".join(sorted(filter_diff))
+            raise ValueError(f"parameter table contains no smoothing parameters for: {strdiff}")
 
     def run(self):
+        """
+        Compute hyperbolic magnitudes based on the parameters determined by `HyperbolicSmoothing`.
+        """
         # get input data
         data = self.get_data('input', allow_missing=True)
         stats = self.get_data('parameters', allow_missing=True)
+        self._check_filters(stats)
         fields = np.zeros(len(data), dtype=np.int)  # placeholder
 
         # intialise the output data
@@ -143,14 +254,12 @@ class HyperbolicMagnitudes(RailStage):
             stats_filt = hyperbolic.fill_missing_stats(stats.loc[mag_col])
 
             # get the zeropoint per source
-            zeropoint = hyperbolic.fields_to_source(
+            zeropoints = hyperbolic.fields_to_source(
                 stats_filt[hyperbolic.Keys.zp], fields, index=data.index)
 
             # compute fluxes from magnitudes
-            mags = data[mag_col].to_numpy()
-            magerrs = data[magerr_col].to_numpy()
-            fluxes = np.exp((zeropoint - mags) / hyperbolic.pogson)
-            fluxerrs = magerrs / hyperbolic.pogson * fluxes
+            fluxes = _compute_flux(data[mag_col], zeropoints)
+            fluxerrs = _compute_flux_error(fluxes, data[magerr_col])
 
             # compute normalised flux
             ref_flux = hyperbolic.fields_to_source(
@@ -170,11 +279,27 @@ class HyperbolicMagnitudes(RailStage):
             output[key_mag] = hyp_mag
             output[key_mag_err] = hyp_mag_err
 
-        # store result
+        # store results
         self.add_data('output', output)
 
     def compute(self, data, parameters):
-        """...
+        """
+        Main method to call. Outputs hyperbolic magnitudes compuated from a set of smoothing
+        parameters and input catalogue with classical magitudes and their respective errors.
+
+        Parameters
+        ----------
+        data : `PqHandle`
+            Input table with magnitude and magnitude error columns as defined in the configuration.
+        parameters : `PqHandle`
+            Table with smoothing parameters per photometric band, determined by
+            `HyperbolicSmoothing`.
+
+        Returns
+        -------
+        output: `PqHandle`
+            Output table containting the values for the smoothing parameter that define the flux
+            scale at which hyperbolic magnitdes transition from linear to logarithmic behaviour.
         """
         self.set_data('input', data)
         self.set_data('parameters', parameters)
