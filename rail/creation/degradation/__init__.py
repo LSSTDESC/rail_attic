8dea030cc65f0cfe15816ddf2e6cc891f28cea19
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2022-05-23 13:09:43 -0700

    Working on lya degrader.

--- rail/creation/degradation/__init__.py
+++ rail/creation/degradation/__init__.py
@@ -1,6 +1,7 @@
 """RailStages that degrade synthetic samples of photometric data"""
 
 from .degrader import *
-from .quantityCut import *
 from .lsst_error_model import *
+from .lya_extinction import *
+from .quantityCut import *
 from .spectroscopic_degraders import *
