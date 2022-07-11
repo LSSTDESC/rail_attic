a612fd7e68c3b8d73b89f9678f99bfc2f4e3f826
torluca <luca.t90@gmail.com>
2022-07-11 11:05:03 +0200

    now the path to the test file is absolute and not relative, test_fsps_sed_gen.fits added to tests/data directory

--- tests/creation/test_sed_generator.py
+++ tests/creation/test_sed_generator.py
@@ -28,7 +28,7 @@ def test_FSPSSedGenerator_bad_tabulated_sfh_params(settings, error):
     with pytest.raises(error):
         DS = RailStage.data_store
         DS.__class__.allow_overwrite = True
-        dummy_io_data = tables_io.read('../data/test_fsps_sed_gen.fits')
+        dummy_io_data = tables_io.read('tests/data/test_fsps_sed_gen.fits')
         sed_generation_test = FSPSSedGenerator.make_stage(name='sed_generator_test', zcontinuous=1,
                                                           add_neb_emission=True, **settings)
         sed_generation_test.add_data('input', dummy_io_data)
@@ -46,7 +46,7 @@ def test_FSPSSedGenerator_bad_tabulated_lsf_params(settings, error):
     with pytest.raises(error):
         DS = RailStage.data_store
         DS.__class__.allow_overwrite = True
-        dummy_io_data = tables_io.read('../data/test_fsps_sed_gen.fits')
+        dummy_io_data = tables_io.read('tests/data/test_fsps_sed_gen.fits')
         sed_generation_test = FSPSSedGenerator.make_stage(name='sed_generator_test', smooth_velocity=False,
                                                           **settings)
         sed_generation_test.add_data('input', dummy_io_data)
@@ -56,7 +56,7 @@ def test_FSPSSedGenerator_bad_tabulated_lsf_params(settings, error):
 def test_FSPSSedGenerator_output_table():
     DS = RailStage.data_store
     DS.__class__.allow_overwrite = True
-    dummy_io_data = tables_io.read('../data/test_fsps_sed_gen.fits')
+    dummy_io_data = tables_io.read('tests/data/test_fsps_sed_gen.fits')
     sed_generation_test = FSPSSedGenerator.make_stage(name='sed_generator_test')
     sed_generation_test.add_data('input', dummy_io_data)
     sed_generation_test.run(physical_units=True)
