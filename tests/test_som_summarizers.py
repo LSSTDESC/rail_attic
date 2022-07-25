7f3fbcb3f7693265a4781e46df4884b301277c9a
sschmidt23 <sschmidt@physics.ucdavis.edu>
2022-07-25 16:47:51 -0700

    unique name for pkl file so it is actually written

--- tests/test_som_summarizers.py
+++ tests/test_som_summarizers.py
@@ -17,28 +17,28 @@ def one_algo(key, inform_class, summarizer_class, summary_kwargs):
     """
     spec_data = DS.read_file('spec_data', TableHandle, testszdata)
     phot_data = DS.read_file('phot_data', TableHandle, testphotdata)
-    informer = inform_class.make_stage(name=f"inform_{key}", model="tmpsom.pkl", **summary_kwargs)
+    informer = inform_class.make_stage(name=f"inform_{key}", model=f"tmpsom_{key}.pkl", **summary_kwargs)
     informer.inform(spec_data)
     summarizerr = summarizer_class.make_stage(name=key, model=informer.get_handle('model'), **summary_kwargs)
     summary_ens = summarizerr.summarize(phot_data, spec_data)
     os.remove(summarizerr.get_output(summarizerr.get_aliased_tag('output'), final_name=True))
     # test loading model by name rather than via handle
-    summarizer2 = summarizer_class.make_stage(name=key, model='tmpsom.pkl')
+    summarizer2 = summarizer_class.make_stage(name=key, model=f'tmpsom_{key}.pkl')
     summ_en = summarizer2.summarize(phot_data, spec_data)
     os.remove(summarizer2.get_output(summarizer2.get_aliased_tag('output'), final_name=True))
-    os.remove("tmpsom.pkl")
+    os.remove(f"tmpsom_{key}.pkl")
     return summary_ens
 
 
 def test_SimpleSOM():
-    summary_config_dict = {}
+    summary_config_dict = {'m_dim': 21, 'n_dim': 21}
     inform_class = simpleSOM.Inform_SimpleSOMSummarizer
     summarizerclass = simpleSOM.SimpleSOMSummarizer
     _ = one_algo("SimpleSOM", inform_class, summarizerclass, summary_config_dict)
 
     
 def test_SimpeSOM_with_mag_and_colors():
-    summary_config_dict = {'use_only_colors': False}
+    summary_config_dict = {'m_dim': 21, 'n_dim' : 21, 'use_only_colors': True}
     inform_class = simpleSOM.Inform_SimpleSOMSummarizer
     summarizerclass = simpleSOM.SimpleSOMSummarizer
     _ = one_algo("SimpleSOM_wmag", inform_class, summarizerclass, summary_config_dict)
