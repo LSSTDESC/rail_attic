e8b4f7c56ac75f9aeb4514f7685e6ee14f7071d8
torluca <luca.t90@gmail.com>
2022-08-11 15:58:33 +0200

    trying alternative way of installing fsps

--- rail/creation/__init__.py
+++ rail/creation/__init__.py
@@ -1,4 +1,6 @@
 """Modules to create synthetic data"""
+import os
+os.environ["SPS_HOME"] = "/opt/hostedtoolcache/Python/fsps"
 from .degradation import *
 from .engines import *
 from .sed_generation import *
--- rail/creation/sed_generation/__init__.py
+++ rail/creation/sed_generation/__init__.py
@@ -1,2 +1,4 @@
+import os
+os.environ["SPS_HOME"] = "/opt/hostedtoolcache/Python/fsps"
 from .generator import *
 from .sed_generator import *
