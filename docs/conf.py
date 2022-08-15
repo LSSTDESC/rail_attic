09eb78ad9c75ce6dba237fa56c253ad9d555151b
Eric Charles <echarles@gmail.com>
2022-08-15 13:16:10 -0700

    fix readthedocs config

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ copyright = '2019-2021, LSST DESC RAIL Contributors'
 author = 'LSST DESC RAIL Contributors'
 
 # The short X.Y version
-from rail import _version
+from rail.core import _version
 version = "%i.%i" % (_version.version_tuple[0], _version.version_tuple[1])
 # The full version, including alpha/beta/rc tags
 release = _version.version
