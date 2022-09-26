640b84aa3d34054c31a7e730aef5f3e1234dc50a
Drew Oldag <awoldag@uw.edu>
2022-09-26 14:37:21 -0700

    Added data validation to brier.py, introduced more tests for full coverage.

--- src/rail/evaluation/metrics/brier.py
+++ src/rail/evaluation/metrics/brier.py
@@ -4,25 +4,91 @@ from .base import MetricEvaluator
 class Brier(MetricEvaluator):
     """ Brier score """
     def __init__(self, prediction, truth):
-        """ Class constructor """
+        """
+        Parameters
+        ----------
+        prediction: NxM array, float
+            Predicted probability for N celestial objects to have a redshift in
+            one of M bins.
+        truth: NxM array, int
+            True redshift values for N celestial objects, where Mth bin for the
+            true redshift will have value 1, all other bins will have a value of
+            0.
+
+        Notes
+        -----
+        Based on https://en.wikipedia.org/wiki/Brier_score#Definition
+        """
         super().__init__(None)
 
-        self._prediction = np.array(prediction)
-        self._truth = np.array(truth)
+        self._prediction = prediction
+        self._truth = truth
 
-    def evaluate(self) -> float:
+    def evaluate(self):
         """
-        Evaluate the Brier score
+        Evaluate the Brier score for N celestial objects.
 
-        Parameters
-        prediction: numpy.ndarray, float
-            predicted class probabilities
-        truth: numpy.ndarray, int
-            true classes mask
-
-        
-
-        Based on https://en.wikipedia.org/wiki/Brier_score#Original_definition_by_Brier
+        Returns
+        -------
+        Brier metric: Nx1 numpy.ndarray, float
+            The result of calculating the Brier metric.
         """
-        
-        return np.average((self._prediction - self._truth)**2)
+
+        self._manipulate_data()
+        self._validate_data()
+        return self._calculate_metric()
+
+    def _manipulate_data(self):
+        """
+        Placeholder for data manipulation as required. i.e. converting from
+        qp.ensamble objects into np.array objects.
+        """
+
+        # Attempt to convert the input variables into np.arrays
+        self._prediction = np.array(self._prediction)
+        self._truth = np.array(self._truth)
+
+    def _validate_data(self):
+        """
+        Strictly for data validation - no calculations or data structure
+        changes.
+
+        Raises
+        ------
+        TypeError if either prediction or truth input could not be converted
+        into a numeric Numpy array
+
+        ValueError if the prediction and truth arrays do not have the same
+        numpy.shape.
+        """
+
+        # Raise TypeError exceptions if the inputs were not translated to
+        # numeric np.arrays
+        if not np.issubdtype(self._prediction.dtype, np.number):
+            raise TypeError("prediction could not be converted to a Numpy array")
+        if not np.issubdtype(self._truth.dtype, np.number):
+            raise TypeError("truth could not be converted to a Numpy array")
+
+        # Raise ValueError if the arrays have different shapes
+        if self._prediction.shape != self._truth.shape:
+            raise ValueError("prediction and truth arrays do not have the same dimensions")
+
+    def _calculate_metric(self):
+        """
+        Calculate the Brier metric for the input data.
+        """
+
+        # If inputs are 1-d arrays adjust axis and retained output dimensions.
+        # Note: It's safe to check just self._prediction, becuase in
+        # _validate_data, we raise an exception if _prediction and _truth do not
+        # have the same shape.
+        AVERAGE_ALONG_ROWS = 1
+        KEEP_DIMS = False
+        if self._prediction.ndim == 1:
+            AVERAGE_ALONG_ROWS = 0
+            KEEP_DIMS = True
+
+        return np.mean(
+            (self._prediction - self._truth)**2,
+            axis=AVERAGE_ALONG_ROWS,
+            keepdims=KEEP_DIMS)
--- tests/test_brier.py
+++ tests/test_brier.py
@@ -4,32 +4,88 @@ from rail.evaluation.metrics.brier import Brier
 
 
 def test_brier_base():
-    pred = [[1,2,3], [1,2,3]]
-    truth = [[1,2,3], [1,2,3]]
+    """
+    Test the base case, ensure output is expected.
+    """
+    pred = [[1,0,0], [1,0,1]]
+    truth = [[1,0,0], [0,1,0]]
     brier_obj = Brier(pred, truth)
     result = brier_obj.evaluate()
-    expected = np.array([0,0])
+    expected = np.array([0,1])
     assert isinstance(result, np.ndarray)
     np.testing.assert_equal(result, expected)
 
+def test_brier_result_has_correct_dimensions():
+    """
+    Verify the dimensionality of the output. Given input of NxM, expect output
+    of Nx1.
+    """
+    pred = [[1,0,0], [0,1,0], [0,0,1]]
+    truth = [[1,0,0], [0,1,0], [0,0,1]]
+    brier_obj = Brier(pred, truth)
+    result = brier_obj.evaluate()
+    np.testing.assert_equal(result.shape, (3,))
+
+def test_brier_base_with_non_integers():
+    """
+    Verify output for non-integer prediction values.
+    """
+    pred = [[0.5,0.5,0]]
+    truth = [[1,0,0]]
+    brier_obj = Brier(pred, truth)
+    result = brier_obj.evaluate()
+    expected = np.array([0.16666667])
+    assert np.isclose(result, expected)
+
+def test_brier_input_arrays_different_sizes():
+    """
+    Verify exception is raised when input arrays are different sizes.
+    """
+    pred = [[1,0,0], [0,1,0]]
+    truth = [[1,0,0], [0,1,0], [0,0,0]]
+    brier_obj = Brier(pred, truth)
+    with pytest.raises(ValueError):
+        _ = brier_obj.evaluate()
+
+def test_brier_with_garbage_prediction_input():
+    """
+    Verify exception is raised when prediction input is non-numeric.
+    """
+    pred = ["foo", "bar"]
+    truth = [[1,0,0],[0,1,0]]
+    brier_obj = Brier(pred, truth)
+    with pytest.raises(TypeError):
+        _ = brier_obj.evaluate()
+
+def test_brier_with_garbage_truth_input():
+    """
+    Verify exception is raised when truth input is non-numeric.
+    """
+    pred = [[1,0,0], [0,1,0]]
+    truth = ["hello sky", "goodbye ground"]
+    brier_obj = Brier(pred, truth)
+    with pytest.raises(TypeError):
+        _ = brier_obj.evaluate()
+
 def test_brier_1d():
-    pred = [[1,2,3]]
-    truth = [[1,2,3]]
+    """
+    Verify 1 dimensional input produced the correct output. This exercises the
+    condition in brier._calculate_metric that changes the axis upon which the
+    np.mean operates.
+    """
+    pred = [1,0,0]
+    truth = [1,0,0]
     brier_obj = Brier(pred, truth)
     result = brier_obj.evaluate()
     expected = np.array([0])
     np.testing.assert_equal(result, expected)
 
-def test_brier_input_arrays_different_sizes():
-    pred = [[1,2,3], [1,2,3]]
-    truth = [[1,2,3], [1,2,3], [1,2,3]]
+def test_brier_1d_result_has_correct_shape():
+    """
+    Verify 1 dimensional input produces a 1 dimensional output.
+    """
+    pred = [1,0,0]
+    truth = [1,0,0]
     brier_obj = Brier(pred, truth)
-    with pytest.raises(ValueError):
-        result = brier_obj.evaluate()
-
-def test_brier_with_garbage_input():
-    pred = [[1,2,3], [1,2,3]]
-    truth = "hello world"
-    brier_obj = Brier(pred, truth)
-    with pytest.raises(ValueError):
-        result = brier_obj.evaluate()
+    result = brier_obj.evaluate()
+    np.testing.assert_equal(result.shape, (1,))
