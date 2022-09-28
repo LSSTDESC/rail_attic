Corrected metric calculation, added "sum to 1" input validation, updated tests.

src/rail/evaluation/metrics/brier.py
@@ -1,3 +1,4 @@
+import logging
 import numpy as np
 from .base import MetricEvaluator
 
@@ -9,7 +10,7 @@ class Brier(MetricEvaluator):
         ----------
         prediction: NxM array, float
             Predicted probability for N celestial objects to have a redshift in
-            one of M bins.
+            one of M bins. The sum of values along each row N should be 1.
         truth: NxM array, int
             True redshift values for N celestial objects, where Mth bin for the
             true redshift will have value 1, all other bins will have a value of
@@ -17,12 +18,13 @@ class Brier(MetricEvaluator):
 
         Notes
         -----
-        Based on https://en.wikipedia.org/wiki/Brier_score#Definition
+        Based on https://en.wikipedia.org/wiki/Brier_score#Original_definition_by_Brier
         """
         super().__init__(None)
 
         self._prediction = prediction
         self._truth = truth
+        self._axis_for_summation = None #axis to sum for metric calculation
 
     def evaluate(self):
         """
@@ -30,7 +32,7 @@ class Brier(MetricEvaluator):
 
         Returns
         -------
-        Brier metric: Nx1 numpy.ndarray, float
+        Brier metric: float in the interval [0,2]
             The result of calculating the Brier metric.
         """
 
@@ -60,35 +62,32 @@ class Brier(MetricEvaluator):
 
         ValueError if the prediction and truth arrays do not have the same
         numpy.shape.
+
+        Warning
+        -------
+        Logs a warning message if the input predictions do not each sum to 1.
         """
 
         # Raise TypeError exceptions if the inputs were not translated to
         # numeric np.arrays
         if not np.issubdtype(self._prediction.dtype, np.number):
-            raise TypeError("prediction could not be converted to a Numpy array")
+            raise TypeError("Input prediction array could not be converted to a Numpy array")
         if not np.issubdtype(self._truth.dtype, np.number):
-            raise TypeError("truth could not be converted to a Numpy array")
+            raise TypeError("Input truth array could not be converted to a Numpy array")
 
         # Raise ValueError if the arrays have different shapes
         if self._prediction.shape != self._truth.shape:
-            raise ValueError("prediction and truth arrays do not have the same dimensions")
+            raise ValueError("Input prediction and truth arrays do not have the same shape")
+
+        # Log a warning if the N rows of the input prediction do not each sum to
+        # 1. Note: For 1d arrays, a sum along axis = 1 will fail, so we set
+        # self._axis_for_summation appropriately for that case
+        self._axis_for_summation = 0 if self._prediction.ndim == 1 else 1
+        if not np.allclose(np.sum(self._prediction, axis=self._axis_for_summation), 1.):
+            logging.warning("Input predictions do not sum to 1.")
 
     def _calculate_metric(self):
         """
         Calculate the Brier metric for the input data.
         """
-
-        # If inputs are 1-d arrays adjust axis and retained output dimensions.
-        # Note: It's safe to check just self._prediction, because in
-        # _validate_data, we raise an exception if _prediction and _truth do not
-        # have the same shape.
-        AVERAGE_ALONG_ROWS = 1
-        KEEP_DIMS = False
-        if self._prediction.ndim == 1:
-            AVERAGE_ALONG_ROWS = 0
-            KEEP_DIMS = True
-
-        return np.mean(
-            (self._prediction - self._truth)**2,
-            axis=AVERAGE_ALONG_ROWS,
-            keepdims=KEEP_DIMS)
+        return np.mean(np.sum((self._prediction - self._truth)**2, axis=self._axis_for_summation))

tests/test_brier.py
@@ -1,30 +1,30 @@
+import logging
 import pytest
 import numpy as np
 from rail.evaluation.metrics.brier import Brier
 
+LOGGER = logging.getLogger(__name__)
 
 def test_brier_base():
     """
     Test the base case, ensure output is expected.
     """
-    pred = [[1,0,0], [1,0,1]]
+    pred = [[1,0,0], [1,0,0]]
     truth = [[1,0,0], [0,1,0]]
     brier_obj = Brier(pred, truth)
     result = brier_obj.evaluate()
-    expected = np.array([0,1])
-    assert isinstance(result, np.ndarray)
-    np.testing.assert_equal(result, expected)
+    expected = 1.
+    assert np.isclose(result, expected)
 
-def test_brier_result_has_correct_dimensions():
+def test_brier_result_is_scalar():
     """
-    Verify the dimensionality of the output. Given input of NxM, expect output
-    of Nx1.
+    Verify output is scalar for input of NxM.
     """
-    pred = [[1,0,0], [0,1,0], [0,0,1]]
+    pred = [[1,0,0], [0,1,0], [0,0.5,0.5]]
     truth = [[1,0,0], [0,1,0], [0,0,1]]
     brier_obj = Brier(pred, truth)
     result = brier_obj.evaluate()
-    np.testing.assert_equal(result.shape, (3,))
+    assert np.isscalar(result)
 
 def test_brier_base_with_non_integers():
     """
@@ -34,7 +34,31 @@ def test_brier_base_with_non_integers():
     truth = [[1,0,0]]
     brier_obj = Brier(pred, truth)
     result = brier_obj.evaluate()
-    expected = np.array([0.16666667])
+    expected = 0.5
+    assert np.isclose(result, expected)
+
+def test_brier_max_result():
+    """
+    Base case where prediction is completely wrong, should produce maximum
+    possible result value, 2.
+    """
+    pred = [[0,1,0], [1,0,0]]
+    truth = [[1,0,0], [0,1,0]]
+    brier_obj = Brier(pred, truth)
+    result = brier_obj.evaluate()
+    expected = 2.
+    assert np.isclose(result, expected)
+
+def test_brier_min_result():
+    """
+    Base case where prediction is perfect, should produce minimum possible
+    result value, 0.
+    """
+    pred = [[1,0,0], [0,1,0]]
+    truth = [[1,0,0], [0,1,0]]
+    brier_obj = Brier(pred, truth)
+    result = brier_obj.evaluate()
+    expected = 0.
     assert np.isclose(result, expected)
 
 def test_brier_input_arrays_different_sizes():
@@ -67,6 +91,32 @@ def test_brier_with_garbage_truth_input():
     with pytest.raises(TypeError):
         _ = brier_obj.evaluate()
 
+def test_brier_prediction_does_not_sum_to_one(caplog):
+    """
+    Verify exception is raised when prediction input rows don't sum to 1 This
+    also verifies that while the total sum of values in the prediction array sum
+    to 2, the individual rows do not, and thus logs a warning
+    """
+    pred = [[1,0.0001,0], [0,0.9999,0]]
+    truth = [[1,0,0], [0,1,0]]
+    LOGGER.info('Testing now...')
+    brier_obj = Brier(pred, truth)
+    with caplog.at_level(logging.WARNING):
+        _ = brier_obj.evaluate()
+    assert "Input predictions do not sum to 1" in caplog.text
+
+def test_brier_1d_prediction_does_not_sum_to_one(caplog):
+    """
+    Verify exception is raised when 1d prediction input rows don't sum to 1
+    """
+    pred = [0.3,0.8,0]
+    truth = [1,0,0]
+    LOGGER.info('Testing now...')
+    brier_obj = Brier(pred, truth)
+    with caplog.at_level(logging.WARNING):
+        _ = brier_obj.evaluate()
+    assert "Input predictions do not sum to 1" in caplog.text
+
 def test_brier_1d():
     """
     Verify 1 dimensional input produced the correct output. This exercises the
@@ -77,15 +127,5 @@ def test_brier_1d():
     truth = [1,0,0]
     brier_obj = Brier(pred, truth)
     result = brier_obj.evaluate()
-    expected = np.array([0])
-    np.testing.assert_equal(result, expected)
-
-def test_brier_1d_result_has_correct_shape():
-    """
-    Verify 1 dimensional input produces a 1 dimensional output.
-    """
-    pred = [1,0,0]
-    truth = [1,0,0]
-    brier_obj = Brier(pred, truth)
-    result = brier_obj.evaluate()
-    np.testing.assert_equal(result.shape, (1,))
+    expected = 0.
+    assert np.isclose(result, expected)