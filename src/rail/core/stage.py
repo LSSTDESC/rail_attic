f93591678ce1d3a87ef55d16662be8ad517fcdcd
Eric Charles <badass@stanford.edu>
2022-10-05 11:26:29 -0700

    fix input_iterator (#267)

--- src/rail/core/stage.py
+++ src/rail/core/stage.py
@@ -325,8 +325,6 @@ class RailStage(PipelineStage):
             These will be passed to the Handle's iterator method
         """
         handle = self.get_handle(tag, allow_missing=True)
-        if not handle.has_data:  #pragma: no cover
-            handle.read()
         if self.config.hdf5_groupname:
             self._input_length = handle.size(groupname=self.config.hdf5_groupname)
             kwcopy = dict(groupname=self.config.hdf5_groupname,
