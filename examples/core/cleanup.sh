452f3883dbf1664ba5029d990aad672d652c3450
Olivia R. Lynn <orl@Olivias-MBP.fios-router.home>
2022-11-21 22:28:40 -0500

    Concept: saving files before making a commit (bash fixing)

--- examples/core/cleanup.sh
+++ examples/core/cleanup.sh
@@ -5,8 +5,7 @@ if [[ -z "${RAILDIR}" ]]; then
    \rm -rf examples/core/output_*
    \rm -rf examples/core/pipe_saved*.yml
 else
-   \rm -rf ${RAILDIR}/core/*.out
-   \rm -rf ${RAILDIR}/core/output_*
-   \rm -rf ${RAILDIR}/core/pipe_saved*.yml
+   \rm -rf "${RAILDIR}"/core/*.out
+   \rm -rf "${RAILDIR}"/core/output_*
+   \rm -rf "${RAILDIR}"/core/pipe_saved*.yml
 fi
-
