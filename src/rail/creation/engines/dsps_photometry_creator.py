cb081d8864c1cbfacdc59901842279e745fbd942
Olivia R. Lynn <olynn@andrew.cmu.edu>
2023-04-26 11:20:54 -0400

    Update dsps_photometry_creator.py - remove temp no-covers in this branch too

--- src/rail/creation/engines/dsps_photometry_creator.py
+++ src/rail/creation/engines/dsps_photometry_creator.py
@@ -75,23 +75,23 @@ class DSPSPhotometryCreator(Creator):
             raise ValueError("The dimensionless Hubble constant {self.config.h} is outside of allowed"
                              " range 0 < h < 1")
 
-        self._b = [None, 0, 0, 0] # pragma: no cover
-        self._calc_rest_mag_vmap = jjit(vmap(_calc_rest_mag, in_axes=self._b)) # pragma: no cover
-        self._c = [None, 0, 0, 0, 0, *[None] * 5] # pragma: no cover
-        self._calc_obs_mag_vmap = jjit(vmap(_calc_obs_mag, in_axes=self._c)) # pragma: no cover
-        self.filter_data = np.load(self.config.filter_data) # pragma: no cover
-        self.filter_names = np.array([key for key in self.filter_data.dtype.fields # pragma: no cover
+        self._b = [None, 0, 0, 0]
+        self._calc_rest_mag_vmap = jjit(vmap(_calc_rest_mag, in_axes=self._b))
+        self._c = [None, 0, 0, 0, 0, *[None] * 5]
+        self._calc_obs_mag_vmap = jjit(vmap(_calc_obs_mag, in_axes=self._c))
+        self.filter_data = np.load(self.config.filter_data)
+        self.filter_names = np.array([key for key in self.filter_data.dtype.fields
                                       if 'wave' in key])
-        self.filter_wavelengths = np.array([self.filter_data[key] for key in self.filter_data.dtype.fields # pragma: no cover
+        self.filter_wavelengths = np.array([self.filter_data[key] for key in self.filter_data.dtype.fields
                                             if 'wave' in key])
-        self.filter_transmissions = np.array([self.filter_data[key] for key in self.filter_data.dtype.fields # pragma: no cover
+        self.filter_transmissions = np.array([self.filter_data[key] for key in self.filter_data.dtype.fields
                                               if 'trans' in key])
-        self.rest_frame_wavelengths = np.load(self.config.rest_frame_wavelengths) # pragma: no cover
-        self.galaxy_redshifts = np.load(self.config.galaxy_redshifts) # pragma: no cover
+        self.rest_frame_wavelengths = np.load(self.config.rest_frame_wavelengths)
+        self.galaxy_redshifts = np.load(self.config.galaxy_redshifts)
 
         if not isinstance(args, dict):  # pragma: no cover
-            args = vars(args) # pragma: no cover
-        self.open_model(**args) # pragma: no cover
+            args = vars(args)
+        self.open_model(**args)
 
     def open_model(self, **kwargs):
         """Load the mode and/or attach it to this Creator
@@ -109,7 +109,7 @@ class DSPSPhotometryCreator(Creator):
             The object encapsulating the trained model.
         """
 
-        model = kwargs.get("rest_frame_sed_models", None) # pragma: no cover
+        model = kwargs.get("rest_frame_sed_models", None)
         if model is None or model == "None":  # pragma: no cover
             self.model = None
             return self.model
@@ -120,8 +120,8 @@ class DSPSPhotometryCreator(Creator):
         if isinstance(model, ModelHandle):  # pragma: no cover
             if model.has_path:
                 self.config["model"] = model.path
-        self.model = self.set_data("model", model) # pragma: no cover
-        return self.model # pragma: no cover
+        self.model = self.set_data("model", model)
+        return self.model
 
     def sample(self, seed: int = None, **kwargs):
         r"""
@@ -148,12 +148,12 @@ class DSPSPhotometryCreator(Creator):
         Finally, the `FitsHandle` associated to the `output` tag is returned.
 
         """
-        self.config["seed"] = seed # pragma: no cover
-        self.config.update(**kwargs) # pragma: no cover
-        self.run() # pragma: no cover
-        self.finalize() # pragma: no cover
-        output = self.get_handle("output") # pragma: no cover
-        return output # pragma: no cover
+        self.config["seed"] = seed
+        self.config.update(**kwargs)
+        self.run()
+        self.finalize()
+        output = self.get_handle("output")
+        return output
 
     def run(self):
         """
@@ -167,26 +167,26 @@ class DSPSPhotometryCreator(Creator):
 
         """
 
-        filter_wavelengths = np.stack((self.filter_wavelengths,) * self.config.n_galaxies, axis=0) # pragma: no cover
-        filter_transmissions = np.stack((self.filter_transmissions,) * self.config.n_galaxies, axis=0) # pragma: no cover
+        filter_wavelengths = np.stack((self.filter_wavelengths,) * self.config.n_galaxies, axis=0)
+        filter_transmissions = np.stack((self.filter_transmissions,) * self.config.n_galaxies, axis=0)
 
-        args = (self.rest_frame_wavelengths, # pragma: no cover
+        args = (self.rest_frame_wavelengths,
                 self.model.reshape((self.config.n_galaxies, len(self.rest_frame_wavelengths))),
                 filter_wavelengths, filter_transmissions)
 
-        rest_frame_absolute_mags = self._calc_rest_mag_vmap(*args).reshape((self.config.n_galaxies, # pragma: no cover
+        rest_frame_absolute_mags = self._calc_rest_mag_vmap(*args).reshape((self.config.n_galaxies,
                                                                             len(self.filter_wavelengths)))
 
-        args = (self.rest_frame_wavelengths, # pragma: no cover
+        args = (self.rest_frame_wavelengths,
                 self.model.reshape((self.config.n_galaxies, len(self.rest_frame_wavelengths))),
                 filter_wavelengths, filter_transmissions, self.galaxy_redshifts,
                 self.config.Om0, self.config.Ode0, self.config.w0, self.config.wa, self.config.h)
 
-        apparent_magnitudes = self._calc_obs_mag_vmap(*args).reshape((self.config.n_galaxies, # pragma: no cover
+        apparent_magnitudes = self._calc_obs_mag_vmap(*args).reshape((self.config.n_galaxies,
                                                                       len(self.filter_wavelengths)))
 
-        idxs = np.arange(1, self.config.n_galaxies + 1, 1, dtype=int) # pragma: no cover
+        idxs = np.arange(1, self.config.n_galaxies + 1, 1, dtype=int)
 
-        output_table = Table([idxs, rest_frame_absolute_mags, apparent_magnitudes], names=('id', 'abs_mags', # pragma: no cover
+        output_table = Table([idxs, rest_frame_absolute_mags, apparent_magnitudes], names=('id', 'abs_mags',
                                                                                            'app_mags'))
-        self.add_data('output', output_table) # pragma: no cover
+        self.add_data('output', output_table)
