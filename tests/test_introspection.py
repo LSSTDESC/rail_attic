165dfd1a165a639b7850fc66b2fa0c6289fa1c06
Eric Charles <badass@stanford.edu>
2023-05-11 10:57:52 -0700

    updated introspection stuff

--- tests/test_introspection.py
+++ tests/test_introspection.py
@@ -1,28 +1,33 @@
 import tempfile
+import pkgutil
+import setuptools
+
+import rail
+from rail.core import RailEnv
 import rail.stages
 
 
 def test_print_rail_packages():
-    rail.stages.print_rail_packages()
+    RailEnv.print_rail_packages()
 
     
 def test_print_rail_namespaces():
-    rail.stages.print_rail_namespaces()
+    RailEnv.print_rail_namespaces()
 
 
 def test_print_rail_modules():
-    rail.stages.print_rail_modules()
+    RailEnv.print_rail_modules()
 
 
 def test_print_rail_namespace_tree():
-    rail.stages.print_rail_namespace_tree()
+    RailEnv.print_rail_namespace_tree()
 
 
 def test_import_and_attach_all():
     rail.stages.import_and_attach_all()
-    rail.stages.print_rail_stage_dict()
+    RailEnv.print_rail_stage_dict()
 
 
 def test_api_rst():
     with tempfile.TemporaryDirectory() as tmpdirname:
-        rail.stages.do_api_rst(tmpdirname)
+        RailEnv.do_api_rst(tmpdirname)
