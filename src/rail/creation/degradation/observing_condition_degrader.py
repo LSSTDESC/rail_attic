f6653b259e0c058747ff73c6c7a8d092c430bc1b
hangqianjun <hangqianjun007@gmail.com>
2023-05-16 09:37:33 -0700

    Test coverage with renameDict

--- src/rail/creation/degradation/observing_condition_degrader.py
+++ src/rail/creation/degradation/observing_condition_degrader.py
@@ -167,6 +167,8 @@ class ObsCondition(Degrader):
         # Check if extra keys are passed
         # get lsst_error_model keys
         lsst_error_model_keys = [field.name for field in fields(LsstErrorParams)]
+        # append renameDict which doesn't seem to be included in this list
+        lsst_error_model_keys.append("renameDict")
         if len(set(self.config["map_dict"].keys()) - set(lsst_error_model_keys)) != 0:
             extra_keys = set(self.config["map_dict"].keys()) - set(lsst_error_model_keys)
             # now we added EBV, which is not in LsstErrorParams:
